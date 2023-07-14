487a1956d8ae715f5aaa47252d3cfdf8c41f8cc7
TSYOGITA <77011543+TSYOGITA@users.noreply.github.com>
2023-07-14 11:41:36 +0530

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,22 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+function togglePopups(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+function togglePopupy(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+function togglePopupt(){
+    document.getElementById("popup-4").classList.toggle("active");
+}
+function togglePopuph(){
+    document.getElementById("popup-5").classList.toggle("active");
+}
+function togglePopupg(){
+    document.getElementById("popup-6").classList.toggle("active");
+}
+
+
+
+
